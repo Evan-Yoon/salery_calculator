40ffd1d8a0a3039cb51cbb5b619d383873ab9da3
User <wlgus1805@naver.com>
2026-03-05 14:02:14 +0900

    test: add comprehensive unit tests for shift salary calculation logic

--- test/shift_calculator_test.dart
+++ test/shift_calculator_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import '../lib/utils/shift_calculator.dart';
+import 'package:shift_salary_calculator/utils/shift_calculator.dart';
 
 void main() {
   group('ShiftCalculator Tests', () {
